abf25da5997b9ef996838d0d6c2fa1cab8f1a442
Alex Feitoza <159736255+AlexFeitoza@users.noreply.github.com>
2024-10-27 19:56:02 -0300

    up

--- script.js
+++ script.js
@@ -16,5 +16,14 @@ text.innerHTML = text.innerHTML.split("").map((char,i)=>
 
 // switch between about buttons
 
-const buttons = document.querySelectorAll(".about-btn buttons");
+const buttons = document.querySelectorAll(".about-btn button");
 const contents = document.querySelectorAll(".content");
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        contents.forEach(content => content.style.display = 'none');
+        contents[index].style.display = 'block';
+        buttons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+    });
+});
